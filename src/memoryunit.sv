ff7c4d83f1b18ca752048a11ca328f471c965ceb
splogdes <95136830+splogdes@users.noreply.github.com>
2022-12-14 22:15:53 +0000

    revert memory unit

--- src/memoryunit.sv
+++ src/memoryunit.sv
@@ -1,5 +1,5 @@
 module memoryunit #(
-    parameter   DATA_WIDTH = 32, ADDRESS_WIDTH = 32, MEMORY_SIZE = 14
+    parameter   DATA_WIDTH = 32, ADDRESS_WIDTH = 32, MEMORY_SIZE = 14, SOURCE_FILE = "datamemory.mem"
 ) (
     input logic     [ADDRESS_WIDTH-1:0] address,
     input logic     [DATA_WIDTH-1:0]    write_data,
@@ -9,33 +9,23 @@ module memoryunit #(
     output logic    [DATA_WIDTH-1:0]    read_data
 );
 
-logic [DATA_WIDTH-1:0] mem_in,mem_out,mem_addr,cache_out;
+logic [DATA_WIDTH-1:0] mem_in,mem_out;
 
-datamemory #(DATA_WIDTH,ADDRESS_WIDTH-2,MEMORY_SIZE) datamemory1(
-    .address(mem_addr),
-    .write_data(write_data),
+datamemory #(DATA_WIDTH,ADDRESS_WIDTH-2,MEMORY_SIZE,SOURCE_FILE) datamemory1(
+    .address(address[ADDRESS_WIDTH-1:2]),
+    .write_data(mem_in),
     .write_enable(write_enable),
     .clk(clk),
     .read_data(mem_out)
 );
 
-// datacontroller #(DATA_WIDTH) data_controller1(
-//     .mem_data_in(write_data),
-//     .mem_data_out(mem_out),
-//     .DATAMEMControl(DATAMEMControl),
-//     .First_2(address[1:0]),
-//     .write_data(mem_in),
-//     .read_data(read_data)
-// );
-
-cache #(DATA_WIDTH,ADDRESS_WIDTH-2,8,3) cache1(
-    .address(address[ADDRESS_WIDTH-1:2]),
-    .mem_data_in(mem_out),
-    .clk(clk),
-    .write_enable(write_enable),
-    .d_out(cache_out),
-    .write_data()
-    .mem_address(mem_addr)
+datacontroller #(DATA_WIDTH) data_controller1(
+    .mem_data_in(write_data),
+    .mem_data_out(mem_out),
+    .DATAMEMControl(DATAMEMControl),
+    .First_2(address[1:0]),
+    .write_data(mem_in),
+    .read_data(read_data)
 );
     
 endmodule
