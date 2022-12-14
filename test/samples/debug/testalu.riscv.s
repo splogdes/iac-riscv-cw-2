83727fdbf06fa4eb2c45e8ef5a5e4d59bb4c01cc
Patrick Beart <patrick.beart@gmail.com>
2022-12-14 21:52:25 +0000

    Simplified alu test program

--- test/samples/debug/testalu.riscv.s
+++ test/samples/debug/testalu.riscv.s
@@ -1,6 +1,9 @@
 addi a0, x0, 0b100
+nop
+nop
+nop
 addi a0, a0, 0b1
-addi a0, a0, 0b10
-addi a4, x0, 0b0
-addi a4, a0, 0b1
-sub a0, x0, a4
+#addi a0, a0, 0b10
+##addi a4, x0, 0b0
+#addi a4, a0, 0b1
+#sub a0, x0, a4
